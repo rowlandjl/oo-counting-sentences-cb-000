54258bec8f66aca14d0b0a6e24705767b97571aa
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-06 15:34:06 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,6 +15,6 @@ class String
   end
 
   def count_sentences
-    self.split(/[.\!\?]/).reject! {|sentence| sentence.empty?}.length
+    self.split(/[.\!\?]/).reject {|sentence| sentence.empty?}
   end
 end
